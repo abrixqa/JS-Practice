a9ecfa9cbdece3d0dc5e3ca583a6a12c5801e4a8
abrixqa <abrix.qa@gmail.com>
2023-01-23 12:55:07 -0600

    JS math challenges.

--- js-practice.js
+++ js-practice.js
@@ -8,3 +8,13 @@ function differenceOfSquares(num1, num2){
     return num1 ** 2 - num2 ** 2;
 }
 
+//Area and perimeter of a triangle
+function triangle(a, b, c){
+    return [a + b + c, ((a + b + c) /2 * ((a + b + c) /2 - a) * ((a + b + c) /2 -  b) * ((a + b + c) /2 - c)) ** 0.5 ];
+}
+
+//Time in seconds
+function time(hour, minutes, seconds){
+    return hour * 60 ** 2 + minutes * 60 + seconds;
+}
+
