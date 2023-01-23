6cca933f9f9f6740ac627ec8904c9a0ac4dcc761
abrixqa <abrix.qa@gmail.com>
2023-01-23 12:45:36 -0600

    JS math challenges

---
+++ js-practice.js
@@ -0,0 +1,10 @@
+// The opposite of a number
+function opposite(num){
+    return -num;
+}
+
+// Difference of squares
+function differenceOfSquares(num1, num2){
+    return num1 ** 2 - num2 ** 2;
+}
+
