0e632187e7573f3f1ab397e8caca497cd7198a77
abrixqa <abrix.qa@gmail.com>
2023-01-24 12:36:22 -0600

    JS math challenges.

--- js-practice.js
+++ js-practice.js
@@ -18,3 +18,36 @@ function time(hour, minutes, seconds){
     return hour * 60 ** 2 + minutes * 60 + seconds;
 }
 
+//Reverse sentence
+function revString(str) {
+    // Create a variable to hold the reversed string
+    let reversed = '';
+    // Iterate through the input string, backwards
+    for (let i = str.length - 1; i >= 0; i--) {
+        // Check if the current character is a letter or not
+        if (str[i].match(/[a-z]/i)) {
+            // If it's a letter, add it to the reversed string
+            reversed += str[i];
+        } else {
+            // If it's not a letter, add the original character to the reversed string
+            reversed += str[i];
+        }
+    }
+    // Iterate through the input string
+    for (let i = 0; i < str.length; i++) {
+        // Check if the current character is uppercase or lowercase
+        if (str[i] === str[i].toUpperCase()) {
+            // If it's uppercase, make the corresponding character in the reversed string uppercase
+            reversed = reversed.slice(0, i) + reversed[i].toUpperCase() + reversed.slice(i + 1);
+        } else {
+            // If it's lowercase, make the corresponding character in the reversed string lowercase
+            reversed = reversed.slice(0, i) + reversed[i].toLowerCase() + reversed.slice(i + 1);
+        }
+    }
+    return reversed;
+}
+console.log(revString('hello World!'));// 'olleh Drlow!'
+console.log(revString('Start for Free')); //"Trats rof Eerf"
+
+
+
